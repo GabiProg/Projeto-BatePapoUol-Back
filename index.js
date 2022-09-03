b2e5e25f1242c132f559484031b501bad597fb5c
GabiProg <gabrielaterese07@gmail.com>
2022-09-03 15:49:56 -0300

    Requisitos da rota POST completos

--- index.js
+++ index.js
@@ -10,16 +10,25 @@ dotenv.config();
 let now = dayjs();
 
 let db;
+
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 mongoClient.connect(() => {
   db = mongoClient.db("bate-papo");
 });
 
 const app = express();
+
 const userSchema = joi.object({
     name: joi.string().required(),
     lastStatus: joi.number().required()
 });
+
+const messageSchema = joi.object({
+    to: joi.string().required(),
+    text: joi.string().required(),
+    type: joi.string().valid('message', 'private_message').required()
+});
+
 app.use(express.json());
 app.use(cors());
 
@@ -36,8 +45,8 @@ app.post('/participants', async (req, res) => {
     }
 
     try{
-        const usedName = await db.collection('participants').findOne({name: name});
-        if(usedName === newName.name){
+        const usedName = await db.collection('participants').findOne({name : name});
+        if(usedName){
             res.sendStatus(409);
             return;
         } 
@@ -68,6 +77,41 @@ app.get('/participants', async (req, res) => {
     } catch (err) {
         res.sendStatus(500);
     }
+});
+
+app.post('/messages', async (req, res) => {
+    const { to, text, type } = req.body;
+    const { user } = req.headers;
+
+    const validation = messageSchema.validate(req.body, {abortEarly: false});
+
+    if(validation.error){
+        const erros = validation.error.details.map(detail => detail.message);
+        res.status(422).send(erros);
+        return;
+    }
+
+    try {
+        const alreadyAparticipant = await db.collection('participants').findOne({name: user});
+        if(alreadyAparticipant){
+            res.sendStatus(422);
+            return;
+        }
+
+        await db.collection('message').insertOne({
+            from: user,
+            to,
+            text,
+            type,
+            time: now.format("HH:mm:ss")
+        });
+
+        res.sendStatus(201);
+
+    } catch (err){
+        res.sendStatus(500);
+    }
+    
 });
 
 app.listen(5000, () => console.log("Listening on port 5000"));
