7cfef5a6bdd44dc0718ff0337cf9a39199887999
GabiProg <gabrielaterese07@gmail.com>
2022-09-04 21:24:28 -0300

    Requisitos da rota status Post completos

--- index.js
+++ index.js
@@ -132,4 +132,23 @@ app.get('/messages', async (req, res) => {
     }
 });
 
+app.post('/status', async (req, res) => {
+    const { user } = req.headers;
+
+    try {
+        const findParticipant = await db.collection('participants').findOne({ name: user });
+        if(!findParticipant){
+            res.sendStatus(404);
+            return;
+        }
+
+        await db.collection('participants').updateOne({ name: user }, {$set: { lastStatus: Date.now() }});
+
+        res.sendStatus(200);
+
+    } catch (err) {
+        res.sendStatus(500);
+    }
+});
+
 app.listen(5000, () => console.log("Listening on port 5000"));
